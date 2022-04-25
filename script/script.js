6d3b3507d55e1847d75cbc2e338b60338bbc9d1c
Шеланов Антон Алексеевич <BT144@bunker-trade.local>
2022-04-25 14:49:42 +0300

    Работа над ошибками

--- script/script.js
+++ script/script.js
@@ -5,32 +5,23 @@ let form = popup.querySelector('.popup__form');
 let closeButton = popup.querySelector('.popup__closeButton');
 let profileName = content.querySelector('.profile-info__name');
 let profileDescription = content.querySelector('.profile-info__description');
-let popupName = popup.querySelector('input[name="name"]');
-let popupDescription = popup.querySelector('input[name="description"]');
-let likeButton = content.querySelector('.element__like');
+let popupName = popup.querySelector('.popup__input_type_name');
+let popupDescription = popup.querySelector('.popup__input_type_description');
 
 function enablePopup() {
     popupName.value = profileName.textContent;
     popupDescription.value = profileDescription.textContent;
-    popup.classList.add('popup_state_visible');
+    popup.classList.add('popup_visible');
 }
 
 function disablePopup() {
-    popup.classList.remove('popup_state_visible');
-}
-
-function saveProfile() {
-    profileName.textContent = popupName.value;
-    profileDescription.textContent = popupDescription.value;
-    disablePopup();
+    popup.classList.remove('popup_visible');
 }
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
-    const nameForm = popupName.value;
-    const infoForm = popupDescription.value;
-    profileName.textContent = nameForm;
-    profileDescription.textContent = infoForm;
+    profileName.textContent = popupName.value;
+    profileDescription.textContent = popupDescription.value;
     disablePopup();
 }
 

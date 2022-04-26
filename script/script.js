c570d6d423ec44537c95d0043235851a71a82107
Шеланов Антон Алексеевич <BT144@bunker-trade.local>
2022-04-26 18:57:20 +0300

    Шесть карточек из коробки

--- script/script.js
+++ script/script.js
@@ -7,6 +7,49 @@ let profileName = content.querySelector('.profile-info__name');
 let profileDescription = content.querySelector('.profile-info__description');
 let popupName = popup.querySelector('.popup__input_type_name');
 let popupDescription = popup.querySelector('.popup__input_type_description');
+let elements = content.querySelector('.elements');
+
+function startElement() {
+    const initialCards = [
+        {
+          name: 'Архыз',
+          link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+        },
+        {
+          name: 'Челябинская область',
+          link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+        },
+        {
+          name: 'Иваново',
+          link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+        },
+        {
+          name: 'Камчатка',
+          link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+        },
+        {
+          name: 'Холмогорский район',
+          link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+        },
+        {
+          name: 'Байкал',
+          link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+        }
+      ]; 
+    for (let i = 0; i < 6; i += 1) {
+        const elementTemplate = document.querySelector('#element').content;
+        const startElement = elementTemplate.querySelector('.element').cloneNode(true);
+        startElement.querySelector('.element__image').src = initialCards[i].link;
+        startElement.querySelector('.element__image').alt = initialCards[i].name;
+        startElement.querySelector('.element__title').textContent = initialCards[i].name;
+        elements.append(startElement);
+        
+        console.log(startElement.querySelector('.element__image').src);
+        console.log(startElement.querySelector('.element__image').alt);
+        console.log(startElement.querySelector('.element__title').value);
+
+    }
+}
 
 function enablePopup() {
     popupName.value = profileName.textContent;
@@ -25,6 +68,7 @@ function formSubmitHandler (evt) {
     disablePopup();
 }
 
+startElement();
 editButton.addEventListener('click', enablePopup);
 closeButton.addEventListener('click', disablePopup);
 form.addEventListener('submit', formSubmitHandler); 

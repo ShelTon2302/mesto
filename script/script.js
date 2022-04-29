a02519aa442eb6431bd518ad3916374056ff65ce
Шеланов Антон Алексеевич <BT144@bunker-trade.local>
2022-04-29 16:22:33 +0300

    edit some errors after review

--- script/script.js
+++ script/script.js
@@ -21,84 +21,82 @@ const popupImg = document.querySelector('.popup_img');
 const closeImgButton = popupImg.querySelector('.popup__close-button_img');
 const popupImgImg = popupImg.querySelector('.popup__img');
 const popupImgDescription = popupImg.querySelector('.popup__description');
+const elementTemplate = document.querySelector('#element').content;
 
 function addElement (src, name) {
-    const elementTemplate = document.querySelector('#element').content;
-    const addElement = elementTemplate.querySelector('.element').cloneNode(true);
-    addElement.querySelector('.element__image').src = src;
-    addElement.querySelector('.element__image').alt = name;
-    addElement.querySelector('.element__title').textContent = name;
-    elements.prepend(addElement);
-}
+    const cloneElement = elementTemplate.querySelector('.element').cloneNode(true);
+    cloneElement.querySelector('.element__image').src = src;
+    cloneElement.querySelector('.element__image').alt = name;
+    cloneElement.querySelector('.element__title').textContent = name;
+    cloneElement.querySelector('.element__like').addEventListener('click', (eve) => changeLikeElementStatus(eve));
+    cloneElement.querySelector('.element__trash').addEventListener('click', (eve) => removeElement(eve));
+    cloneElement.querySelector('.element__image').addEventListener('click', (eve) => enablePopupImg(eve));
+    return cloneElement;
+};
 
-function addElementFunction(kol) {
-    elementLike = elements.querySelectorAll('.element__like');
-    elementTrash = elements.querySelectorAll('.element__trash');
-    elementImg = elements.querySelectorAll('.element__image');
-    for (let i = 0; i < kol; i++) {
-        elementLike[i].addEventListener('click', (eve) => changeLikeElementStatus(eve));
-        elementTrash[i].addEventListener('click', (eve) => elementRemove(eve));
-        elementImg[i].addEventListener('click', (eve) => enablePopupImg(eve));
-    };
-}
+function addElements (src,name) {
+    elements.prepend(addElement(src, name));
+};
 
 function startElement() {
     for (let i = 0; i < 6; i ++) {
-        addElement(initialCards[i].link, initialCards[i].name);
-    }
-    addElementFunction(6);
-}
+        addElements(initialCards[i].link, initialCards[i].name);
+    };
+};
 
 function changeLikeElementStatus(eve) {
     eve.target.classList.toggle('element__like_active');
-}
+};
 
-function elementRemove(eve) {
+function removeElement(eve) {
     eve.target.closest('.element').remove();
-}
+};
 
-function EnableDisablePopup(popup) {
-    popup.classList.toggle('popup_visible');
-}
+function enablePopup(popup) {
+    popup.classList.add('popup_visible');
+};
+
+function disablePopup(popup) {
+    popup.classList.remove('popup_visible');
+};
+
 
 function enablePopupEditProfile() {
     popupEditProfileName.value = profileName.textContent;
     popupEditProfileDescription.value = profileDescription.textContent;
-    EnableDisablePopup(popupEditProfile);
-}
+    enablePopup(popupEditProfile);
+};
 
 function enablePopupAddElement() {
     formAddElement.reset();
-    EnableDisablePopup(popupAddElement);
-}
+    enablePopup(popupAddElement);
+};
 
 function enablePopupImg(eve) {
     popupImgImg.src = eve.target.src;
     popupImgImg.alt = eve.target.alt;
     popupImgDescription.textContent = eve.target.alt;
-    EnableDisablePopup(popupImg);
-}
-
+    enablePopup(popupImg);
+};
 
 function editProfileFormSubmitHandler (evt) {
     evt.preventDefault();
     profileName.textContent = popupEditProfileName.value;
     profileDescription.textContent = popupEditProfileDescription.value;
-    EnableDisablePopup(popupEditProfile);
-}
+    disablePopup(popupEditProfile);
+};
 
 function addElementFormSubmitHandler (evt) {
     evt.preventDefault();
-    addElement(popupAddElementSrc.value, popupAddElementName.value);
-    addElementFunction(1);
-    EnableDisablePopup(popupAddElement);
-}
+    addElements(popupAddElementSrc.value, popupAddElementName.value);
+    disablePopup(popupAddElement);
+};
 
 startElement();
 editButton.addEventListener('click', enablePopupEditProfile);
 addButton.addEventListener('click', enablePopupAddElement);
-closeEditProfileButton.addEventListener('click', () => EnableDisablePopup(popupEditProfile));
-closeAddElementButton.addEventListener('click', () => EnableDisablePopup(popupAddElement));
-closeImgButton.addEventListener('click', () => EnableDisablePopup(popupImg));
+closeEditProfileButton.addEventListener('click', () => disablePopup(popupEditProfile));
+closeAddElementButton.addEventListener('click', () => disablePopup(popupAddElement));
+closeImgButton.addEventListener('click', () => disablePopup(popupImg));
 formEditProfile.addEventListener('submit', editProfileFormSubmitHandler);
 formAddElement.addEventListener('submit', addElementFormSubmitHandler);
